07baf89c3e9bd461488ec4a7276c5a80d37750d2
David Stroud <david@dwstroud.com>
2016-02-01 18:01:15 +0000

    Initial files for the first working version

--- udemy-03-multiples/ViewController.swift
+++ udemy-03-multiples/ViewController.swift
@@ -10,16 +10,85 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    var multiplyBy = 0
+    var currentTotal = 0
+    var previousTotal = 0
+    var maxTotal = 100
+    
+    @IBOutlet weak var logoImg: UIImageView!
+    @IBOutlet weak var backgroundImg: UIImageView!
+    @IBOutlet weak var playBtn: UIButton!
+    @IBOutlet weak var addBtn: UIButton!
+    @IBOutlet weak var multiplyingTextLabel: UILabel!
+    @IBOutlet weak var multiplyByInputField: UITextField!
+    
+    @IBAction func onPlayBtnPressed(sender: UIButton!) {
+        
+        // make sure something has been entered in the 'multiply by' text field
+        if multiplyByInputField.text != nil && multiplyByInputField.text != "" {
+            
+            // hide front screen elements and show game screen elements
+            showGameScreen()
+            
+            // set initial variables for game
+            multiplyBy = Int(multiplyByInputField.text!)!
+            previousTotal = 0
+            increaseCurrentTotal()
+            
+            // set initial text display
+            setMultiplyingTextLabel()
+
+        }
+        
+    }
+    
+    @IBAction func onAddBtnPressed(sender: UIButton!) {
+        
+        // see if we've reached our max value, if so, reset
+        if ((currentTotal + multiplyBy) > maxTotal) {
+            resetGame()
+        } else {
+            // set 'previousTotal', add the multiple to 'currentTotal' and set the label to display the result
+            previousTotal = currentTotal
+            increaseCurrentTotal()
+            setMultiplyingTextLabel()
+        }
+        
+    }
+    
+    func setMultiplyingTextLabel() {
+        multiplyingTextLabel.text = "\(previousTotal) + \(multiplyBy) = \(currentTotal)"
+
+    }
+    
+    func resetGame() {
+        
+        logoImg.hidden = false
+        playBtn.hidden = false
+        multiplyByInputField.hidden = false
+        addBtn.hidden = true
+        multiplyingTextLabel.hidden = true
+        
+        currentTotal = 0
+        previousTotal = 0
+        multiplyBy = 0
+    
+    }
+    
+    func showGameScreen() {
+        
+        logoImg.hidden = true
+        playBtn.hidden = true
+        multiplyByInputField.hidden = true
+        addBtn.hidden = false
+        multiplyingTextLabel.hidden = false
+        
+    }
+    
+    func increaseCurrentTotal() {
+        currentTotal = currentTotal + multiplyBy
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
 }
 
